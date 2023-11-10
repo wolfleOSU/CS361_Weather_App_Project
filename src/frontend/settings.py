0fbb8f88abd5fa8e2d705333f3079886874e591d
felixmoren-d <133543308+felixmoren-d@users.noreply.github.com>
2023-11-09 16:49:54 -0800

    Settings architecture

---
+++ src/frontend/settings.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+class settings:
+    def __init__(self, default):
+        #initialize settings
+        #fill in default settings
+        #create display boxes for settings
+
+    def celsius_clicked(self, isclicked):
+        #function for when celsius button is clicked
+        #runs when button is clicked and should change values to celsius
+    
+    def fahrenheit_clicked(self, isclicked):
+        #function for when fahrenheit button is clicked
+        #runs when button is clicked and should change values to fahrenheit
+    
+    def warm_clicked(self, isclicked):
+        #function for when warm setting is clicked
+        #runs when button is clicked and should change the theme to warm
+    
+    def cool_clicked(self, isclicked):
+        #functions for when cool setting is clicked
+        #runs when button is clicked and should change the theme to cool
