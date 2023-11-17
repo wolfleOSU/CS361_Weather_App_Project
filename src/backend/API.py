d572d25787baa9c50ecfc9631998dbba8e452bae
Seiji K <103206807+seijikoenigsberg@users.noreply.github.com>
2023-11-17 11:57:35 -0800

    Hopefully final commit :)

--- src/backend/API.py
+++ src/backend/API.py
@@ -1,6 +1,5 @@
 import requests
-import sys
-from datetime import datetime, timedelta
+from datetime import datetime
 
 
 class current_Weather:
@@ -9,21 +8,25 @@ class current_Weather:
 
         self.url = f'http://api.weatherapi.com/v1/current.json?key={self.API_Key}&q={location}'
         self.response = requests.get(self.url)
+        self.data = {}
 
-    def print_weather(self):
+    def fetch_weather_data(self):
         if self.response.status_code == 200:
             data = self.response.json()
             location = data['location']
             current = data['current']
 
-            print(f"Weather in {location['name']}, {location['region']}, {location['country']}")
-            print(f"Local Time: {location['localtime']}")
-            print(f"Condition: {current['condition']['text']}")
-            print(f"Temperature: {current['temp_c']}°C")
-            print(f"Feels Like: {current['feelslike_c']}°C")
-            print(f"Wind Speed: {current['wind_kph']} km/h")
-        else:
-            print("Failed to fetch weather data")
+            self.data = {
+                'Location': f"{location['name']}, {location['region']}, {location['country']}",
+                'Local Time': location['localtime'],
+                'Condition': current.get('condition', {}).get('text'),
+                'Temperature': int(current.get('temp_c')),
+                'Feels Like': int(current.get('feelslike_c')),
+                'Wind Speed': int(current.get('wind_mph'))
+            }
+
+    def get_weather_data(self):
+        return self.data
 
 
 class geo_Location: 
@@ -67,9 +70,10 @@ class weekly_Forcast:
         self.lattitude = lattitude
         self.longitude = longitude
         self.url = f'https://api.weather.gov/points/{self.lattitude},{self.longitude}'
+        self.data = {}
 
 
-    def print_weather(self):
+    def fetch_weather_data(self):
         response = requests.get(self.url)
 
         if response.status_code == 200:
@@ -77,10 +81,6 @@ class weekly_Forcast:
             
             # Extract the location name
             location_name = data['properties']['relativeLocation']['properties']['city']
-            
-            # Print the location name
-            print()
-            print(f"Location Name: {location_name}")
             
 
             # Extract the grid point URL and forecast hourly URL
@@ -92,24 +92,35 @@ class weekly_Forcast:
             if forecast_response.status_code == 200:
                 forecast_data = forecast_response.json()
                 
-                # Extract and print relevant weather information
                 properties = forecast_data['properties']
                 periods = properties['periods']
-                print("{:<20} {:<15} {:<45} {:<20} {:<20} {:<20} {:<15}".format("Time", "Temperature", "Conditions", "Wind Speed", "Wind Direction", "Humidity", "Dew Point"))
+                
                 for period in periods:
-                    time = period['name']
-                    temperature = f"{period['temperature']}°F"
-                    conditions = period['shortForecast']
-                    wind_speed = period['windSpeed']
-                    wind_direction = period['windDirection']
-                    humidity = f"{period['relativeHumidity']['value']}%"
-                    dew_point = f"{round(period['dewpoint']['value'], 2)}°C"
+                    time_string = period['startTime']
+                    time_convert = datetime.fromisoformat(time_string)
+                    time = time_convert.strftime("%m/%d/%y %H:%M")
+                    temperature = int(f"{period.get('temperature', None)}")
+                    conditions = period.get('shortForecast', None)
+                    wind_speed_str = period.get('windSpeed', None)
+                    wind_speed = int(wind_speed_str.split(' ')[0]) if wind_speed_str else None
+                    wind_direction = period.get('windDirection', None)
+                    humidity = f"{period['relativeHumidity']['value']}%" if 'relativeHumidity' in period else None
+                    dew_point_c = f"{round(period['dewpoint']['value'], 2)}" if 'dewpoint' in period else None
+                    dew_point = None
+                    if dew_point_c is not None:
+                        dew_point = round((float(dew_point_c) * 9/5) + 32, 2)
 
-                    print("{:<20} {:<15} {:<45} {:<20} {:<20} {:<20} {:<15}".format(time, temperature, conditions, wind_speed, wind_direction, humidity, dew_point))
-            else:
-                print("Failed to fetch weather forecast data")
-        else:
-            print("Failed to fetch grid point data")
+                    # Organize data by type, initializing with None
+                    self.data.setdefault('Time', []).append(time)
+                    self.data.setdefault('Temperature', []).append(temperature)
+                    self.data.setdefault('Conditions', []).append(conditions)
+                    self.data.setdefault('Wind Speed', []).append(wind_speed)
+                    self.data.setdefault('Wind Direction', []).append(wind_direction)
+                    self.data.setdefault('Humidity', []).append(humidity)
+                    self.data.setdefault('Dew Point', []).append(dew_point)
+
+    def get_weather_data(self):
+        return self.data
 
 class hourly_Forcast:
     def __init__(self, grid_x, grid_y, grid_ID):
@@ -117,44 +128,61 @@ class hourly_Forcast:
         self.grid_y = grid_y
         self.grid_ID = grid_ID
         self.url = f'https://api.weather.gov/gridpoints/{self.grid_ID}/{self.grid_x},{self.grid_y}/forecast/hourly'
+        self.data = {}
 
-    def print_weather(self):
+    def fetch_weather_data(self):
         forecast_response = requests.get(self.url)
         
         if forecast_response.status_code == 200:
             forecast_data = forecast_response.json()
-            
-            # Extract and print relevant weather information
             properties = forecast_data['properties']
             periods = properties['periods']
-            print()
-            print("{:<20} {:<15} {:<45} {:<20} {:<20} {:<20} {:<15}".format("Time", "Temperature", "Conditions", "Wind Speed", "Wind Direction", "Humidity", "Dew Point"))
+
             for period in periods:
                 time_string = period['startTime']
                 time_convert = datetime.fromisoformat(time_string)
                 time = time_convert.strftime("%m/%d/%y %H:%M")
-                temperature = f"{period['temperature']}°F"
-                conditions = period['shortForecast']
-                wind_speed = period['windSpeed']
-                wind_direction = period['windDirection']
-                humidity = f"{period['relativeHumidity']['value']}%"
-                dew_point = f"{round(period['dewpoint']['value'], 2)}°C"
+                temperature = int(f"{period.get('temperature', None)}")
+                conditions = period.get('shortForecast', None)
+                wind_speed_str = period.get('windSpeed', None)
+                wind_speed = int(wind_speed_str.split(' ')[0]) if wind_speed_str else None
+                wind_direction = period.get('windDirection', None)
+                humidity = f"{period['relativeHumidity']['value']}%" if 'relativeHumidity' in period else None
+                dew_point_c = f"{round(period['dewpoint']['value'], 2)}" if 'dewpoint' in period else None
+                dew_point = None
+                if dew_point_c is not None:
+                    dew_point = round((float(dew_point_c) * 9/5) + 32, 2)
 
-                print("{:<20} {:<15} {:<45} {:<20} {:<20} {:<20} {:<15}".format(time, temperature, conditions, wind_speed, wind_direction, humidity, dew_point))
-        else:
-            print("Failed to fetch weather forecast data")
+                # Organize data by type, initializing with None
+                self.data.setdefault('Time', []).append(time)
+                self.data.setdefault('Temperature', []).append(temperature)
+                self.data.setdefault('Conditions', []).append(conditions)
+                self.data.setdefault('Wind Speed', []).append(wind_speed)
+                self.data.setdefault('Wind Direction', []).append(wind_direction)
+                self.data.setdefault('Humidity', []).append(humidity)
+                self.data.setdefault('Dew Point', []).append(dew_point)
 
+    def get_weather_data(self):
+        return self.data
+
+class initialize:
+    def __init__(self, location):
+        self.location = location
+        self.current = None
+        self.daily = None
+        self.hourly = None
+        weather = current_Weather(location)
+        weather.fetch_weather_data()
+        self.current = weather.data
+        geolocation = geo_Location(location)
+        forecast = weekly_Forcast(geolocation.lattitude, geolocation.longitude)
+        forecast.fetch_weather_data()
+        self.daily = forecast.data
+        API = NWS_API(geolocation.lattitude, geolocation.longitude)
+        hourly = hourly_Forcast(API.grid_x, API.grid_y, API.grid_ID)
+        hourly.fetch_weather_data()
+        self.hourly = hourly.data
+
+    
         
-
-
-location = sys.argv[1]
-weather = current_Weather(location)
-weather.print_weather()
-geolocation = geo_Location(location)
-print(geolocation.lattitude, geolocation.longitude)
-forecast = weekly_Forcast(geolocation.lattitude, geolocation.longitude)
-forecast.print_weather()
-API = NWS_API(geolocation.lattitude, geolocation.longitude)
-hourly = hourly_Forcast(API.grid_x, API.grid_y, API.grid_ID)
-hourly.print_weather()
         
